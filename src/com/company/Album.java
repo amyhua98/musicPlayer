27828729b087b1af6ec71b3c1385b8f90de525d2
Amy Hua <aymay98@gmail.com>
2021-04-08 13:01:41 -0500

    Find and add song

--- src/com/company/Album.java
+++ src/com/company/Album.java
@@ -12,5 +12,22 @@ public class Album {
         this.artist = artist;
         this.songs = new ArrayList<Song>();
     }
+
+    public boolean addSong(String title, double duration){
+        if (findSong(title) == null){
+            this.songs.add(new Song(title, duration));
+            return true;
+        }
+        return false;
+    }
+
+    public Song findSong(String title){
+        for (Song song: this.songs){
+            if(song.getTitle().equals(title)){
+                return song;
+            }
+        }
+        return null;
+    }
     
 }
